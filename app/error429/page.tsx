5c51d9c234013b86fadeca466438750a8a659012
KimPuro <puro@protonmail.ch>
2024-06-02 22:12:44 +0900

    feat : add test user

--- app/error429/page.tsx
+++ app/error429/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
           <p className="text-base font-semibold text-cyan-500">Error</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">에러 페이지 입니다.</h1>
           <p className="mt-6 text-base leading-7 text-gray-600">죄송합니다. 이 페이지의 이메일 전송 한계가 초과되었습니다.<br/>테스트용 계정으로 로그인 해보세요.
-          <br/>puro@protonmail.ch <br/>password</p>
+          <br/>test@kimpuro.com <br/>password</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/"
