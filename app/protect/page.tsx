0ab7ab16dd4a0895887ad22b8afe7021e33ebe75
KimPuro <puro@protonmail.ch>
2024-06-02 18:04:12 +0900

    feat : add mypage

--- app/protect/page.tsx
+++ app/protect/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
 
 export default async function PrivatePage() {
   const supabase = createClient()
@@ -10,5 +11,21 @@ export default async function PrivatePage() {
     redirect('/login')
   }
 
-  return <p>이 페이지에는 로그인한 사용자만 접속할 수 있습니다. 당신의 이메일은 {data.user.email}</p>
+  return (
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-cyan-500">Protect</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">마이 페이지 입니다.</h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          당신의 이메일은 {data.user.email}</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="rounded-md bg-cyan-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </Link>
+          </div>
+      </div>
+    </main>)
 }
--- components/LoginOut.tsx
+++ components/LoginOut.tsx
@@ -30,6 +30,12 @@ export default async function LoginOut() {
 else {
   return (<div className="flex flex-1 items-center justify-end gap-x-6">
     <Link
+      href="/protect"
+      className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
+    >
+      Mypage
+    </Link>
+    <Link
       href="/logout"
       className="rounded-md bg-cyan-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
