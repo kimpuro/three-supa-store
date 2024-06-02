cc3a512033c521edf6926c6d1f6d3867783ea265
KimPuro <puro@protonmail.ch>
2024-06-02 19:53:12 +0900

    fix : fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: "KimPuro's Shoe Store",
+  description: '김동은의 신발 스토어 입니다.',
 }
 
 export default function RootLayout({
